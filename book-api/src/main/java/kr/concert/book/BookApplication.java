4b4674fb327a9d379ffaaa8b0d63972b056e4e41
UHeeJoon <yhjyhj3855@gmail.com>
2025-06-06 17:52:29 +0900

    feat: infra setup
    mysql(primary - replica), redis(primary - replica) cluster 환경으로 변환될 가능성 있음, prometheus grafana, kafka 기본 환경 구축

---
+++ book-api/src/main/java/kr/concert/book/BookApplication.java
@@ -0,0 +1,17 @@
+package kr.concert.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * main
+ *
+ * @author :Uheejoon
+ * @date :2025-06-04 오전 12:21
+ */
+@SpringBootApplication
+public class BookApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookApplication.class, args);
+    }
+}
