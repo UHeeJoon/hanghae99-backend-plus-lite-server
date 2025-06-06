035d8f0ef710b2f9e19a5de9d811841346516eba
UHeeJoon <yhjyhj3855@gmail.com>
2025-06-06 21:10:06 +0900

    [#12] feat: flyway추가 및 init.sql 생성
    jpa ddl은 none 또는 validation으로 사용 하며, db 관련 작업은 flyway 사용

--- book-api/build.gradle.kts
+++ book-api/build.gradle.kts
@@ -6,6 +6,10 @@ dependencies {
     // DB
     runtimeOnly("com.mysql:mysql-connector-j")
 
+    // flyway
+    implementation("org.flywaydb:flyway-core")
+    implementation("org.flywaydb:flyway-mysql")
+
     // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.boot:spring-boot-testcontainers")
